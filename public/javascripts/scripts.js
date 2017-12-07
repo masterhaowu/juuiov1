102864c4a0d0c4cbe082692c464c6b81cde9e419
fermi fang <fermi.fang@gmail.com>
2017-12-07 21:56:11 +0800

    version 7

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,27 @@
+//navbar color change on scroll
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $(".header").height()){
+            $(".navbar").css({"background-color":"black"});   
+        }
+        else{
+            $(".navbar").css({"background-color":"transparent"});
+        }
+    })
+})
+
+
+
+
+//navbar button angle turn when clicked
+$(document).ready(function(){
+    var open = false;
+    $(".navbar-toggler").click(function(){
+        if(open){
+            icon.className = 'fa fa-angle-down';  
+          } else{
+            icon.className = 'fa fa-angle-down open';
+          }          
+          open = !open;       
+    })
+})
