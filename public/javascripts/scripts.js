4b25a8ffe620597442494443449df95810af7a1e
fermi fang <fermi.fang@gmail.com>
2017-12-18 23:04:36 +0800

    version 19 facebook - potential bug not fixed - starting designs - navbar name image problem unsolved 2

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,23 +1,5 @@
 //navbar color change on scroll
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > $(".header").height()){
-            $(".navbar").css({"background-color":"white"});
-            $(".navbar").css({"box-shadow":"0 8px 6px -6px"});            
-            $("#icon").css({"color":"black"});               
-            $(".nav-logo-white").css({"display":"none"})
-            $(".nav-logo-black").css({"display":"inline"})      
-        }
-        else{
-            $(".navbar").css({"background-color":"transparent"});
-            $(".navbar").css({"box-shadow":"none"});
-            $("#icon").css({"color":"white"});
-            $(".nav-logo-white").css({"display":"inline"})     
-            $(".nav-logo-black").css({"display":"none"}) 
-            
-        }
-    })
-})
+
 
 //set position top
 $(document).ready(function(){
