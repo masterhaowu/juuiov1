5e2b258a1be6799f1b5cc9e7402a357fb3ab463e
fermi fang <fermi.fang@gmail.com>
2017-12-26 17:20:34 +0800

    version 21 - property listing build 4 - bugs check log

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -11,35 +11,30 @@ $(document).ready(function(){
 })
 
 $(document).ready(function () {
-
-window.setTimeout(function() {
-    $(".alert").alert('close')
-}, 2000);
+    window.setTimeout(function() {
+        $(".alert").alert('close')
+    }, 2000);
 
 });
 
 //show reset modal
 $(document).ready(function() {
-
     if(window.location.href.indexOf('#resetYourPasswordModal') != -1) {
       $('#resetYourPasswordModal').modal('show');
     }
-  
 });
   
 
 //stop add-property button going to footer
 $(document).ready(function() {
-
     $(window).scroll(function(){
-        if($('#add-property').offset().top + $('#add-property').height() >= $('.footer').offset().top - 60){
-            $('#add-property').css('color', 'red');
+        if($("#add-property").position().top + $("#add-property").height() >= $('.footer').position().top){
+            $("#add-property").css('color', 'red');
         }
-
 
         else{            
             $('#add-property').css('color', 'blue'); 
         }   
-
     }
 }
+
